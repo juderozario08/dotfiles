3ecc77d377b99b7471da5439143a8814ace7305e
Jude Rozario <juderzro08@gmail.com>
2024-06-16 08:06:00 -0400

    First Commit

--- config/nvim/lua/jude/lazy/chunks.lua
+++ config/nvim/lua/jude/lazy/chunks.lua
@@ -3,30 +3,20 @@ return {
     config = function()
         require("hlchunk").setup({
             chunk = {
+                enable = false,
+                notify = true,
+            },
+            indent = {
                 enable = true,
                 notify = true,
                 use_treesitter = false,
-                chars = {
-                    horizontal_line = "─",
-                    vertical_line = "│",
-                    left_top = "╭",
-                    left_bottom = "╰",
-                    right_arrow = ">",
-                },
                 style = {
-                    { fg = "#6DC6E3" }, -- Dim Yellow
+                    { fg = "#333333" },
                 },
-                textobject = "",
-                max_file_size = 1024 * 1024,
-                error_sign = true,
             },
-            indent = { enable = false },
             line_num = {
-                enable = true,
-                use_treesitter = false,
-                style = "#808000", -- Yellow
+                enable = false,
             },
-
             blank = { enable = false },
         })
     end,
