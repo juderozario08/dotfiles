d817fe0cea40b56dd21bd606bfca20326dff2b20
Jude Rozario <juderzro08@gmail.com>
2024-06-17 07:40:57 -0400

    First Commit

--- config/nvim/lua/jude/lazy/go.lua
+++ config/nvim/lua/jude/lazy/go.lua
@@ -1,16 +1,36 @@
 return {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-        "ray-x/guihua.lua",
-        "neovim/nvim-lspconfig",
-        "nvim-treesitter/nvim-treesitter",
+    {
+        "olexsmir/gopher.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "ray-x/go.nvim",
+                dependencies = {
+                    "ray-x/guihua.lua",
+                    "neovim/nvim-lspconfig",
+                    "nvim-treesitter/nvim-treesitter",
+                },
+                config = function()
+                    require("go").setup()
+                    require("go.format").gofmt()
+                    require("go.format").goimports()
+                end,
+                event = { "CmdlineEnter" },
+                ft = { "go", "gomod" },
+                build = ':lua require("go.install").update_all_sync()',
+            },
+        },
+        config = function()
+            require("gopher").setup({
+                commands = {
+                    go = "go",
+                    gomodifytags = "gomodifytags",
+                    gotests = "~/go/bin/gotests",
+                    impl = "impl",
+                    iferr = "iferr",
+                },
+            })
+        end,
     },
-    config = function()
-        require("go").setup()
-        require("go.format").gofmt()
-        require("go.format").goimports()
-    end,
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()',
 }
