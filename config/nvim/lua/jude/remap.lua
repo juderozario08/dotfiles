897714a941a98e055cf3a767f6daf8c2634a0f34
Jude Rozario <juderzro08@gmail.com>
2024-06-26 22:16:24 -0400

    First Commit

--- config/nvim/lua/jude/remap.lua
+++ config/nvim/lua/jude/remap.lua
@@ -1,17 +1,21 @@
 vim.g.mapleader = " "
 
 vim.keymap.set(
-	"n",
-	"<leader>e",
-	":Neotree action=focus source=filesystem float reveal toggle=true<CR>",
-	{ noremap = true, silent = true }
+    "n",
+    "<leader>e",
+    ":Neotree action=focus source=filesystem float reveal toggle=true<CR>",
+    { noremap = true, silent = true }
 )
 vim.keymap.set(
-	"n",
-	"<leader>E",
-	":Neotree action=focus source=buffers float reveal toggle=true<CR>",
-	{ noremap = true, silent = true }
+    "n",
+    "<leader>E",
+    ":Neotree action=focus source=buffers float reveal toggle=true<CR>",
+    { noremap = true, silent = true }
 )
+vim.keymap.set("n", "<M-_>", ":resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<M-+>", ":resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-_>", ":only<CR>", { noremap = true, silent = true })
+
 vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>Q", ":q!<CR>", { noremap = true, silent = true })
 
@@ -36,7 +40,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>", { noremap = true, silent = true })
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>w", ":w!<CR>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>//gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>j", "<cmd>cnext<CR>zz", { noremap = true, silent = true })
