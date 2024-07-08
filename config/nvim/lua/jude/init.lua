a786f91a40830111428962d5f23396aa0b8c312e
Jude Rozario <juderzro08@gmail.com>
2024-07-07 22:00:11 -0400

    First Commit

--- config/nvim/lua/jude/init.lua
+++ config/nvim/lua/jude/init.lua
@@ -24,17 +24,13 @@ autocmd("LspAttach", {
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
         vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>ds", vim.lsp.buf.document_symbol, opts)
         vim.keymap.set("n", "<leader>fo", function()
             vim.lsp.buf.format()
-            vim.api.nvim_command(":w")
+            vim.api.nvim_command(':w')
         end, opts)
     end,
 })
