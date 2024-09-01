b29e1e716abc9e4bc6a219298664891705973cbe
Jude Rozario <juderzro08@gmail.com>
2024-08-31 23:14:09 -0400

    Updated settings

--- config/nvim/lua/jude/init.lua
+++ config/nvim/lua/jude/init.lua
@@ -28,14 +28,10 @@ autocmd("LspAttach", {
         vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
         vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, opts)
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>=", function()
-            vim.lsp.buf.format()
-            vim.api.nvim_command(":w")
-        end, opts)
-        vim.keymap.set("n", "<leader>fo", [[mzgg=G'z:w<CR>]], opts)
+        vim.keymap.set("n", "<leader>=", vim.lsp.buf.format, opts)
         vim.keymap.set("n", "<leader>fo", function()
             vim.lsp.buf.format()
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("mzgg=G'z:w<CR>", true, false, true), "n", true)
+            vim.cmd(":w")
         end, opts)
     end,
 })
@@ -51,23 +47,6 @@ autocmd({ "VimEnter" }, {
     pattern = "*",
     command = [[Copilot disable]],
 })
-
---[[ autocmd("BufWritePre", {
-    pattern = "*.go",
-    callback = function()
-        local params = vim.lsp.util.make_range_params()
-        params.context = { only = { "source.organizeImports" } }
-        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-                                                    for cid, res in pairs(result or {}) do
-            for _, r in pairs(res.result or {}) do
-                if r.edit then
-                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
-                end
-            end
-        end
-    end,
-}) ]]
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
