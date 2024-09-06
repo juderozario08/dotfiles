b1f3c9150484f128dfefae7748fff6f9d38a2384
Jude Rozario <juderzro08@gmail.com>
2024-09-06 08:55:06 -0400

    Updated

--- config/nvim/lua/jude/lazy/lspconfig.lua
+++ config/nvim/lua/jude/lazy/lspconfig.lua
@@ -1,50 +1,50 @@
 return {
-	"williamboman/mason.nvim",
-	lazy = false,
-	dependencies = {
-		{
-			"williamboman/mason-lspconfig.nvim",
-			opts = {
-				auto_install = true,
-			},
-		},
-		"neovim/nvim-lspconfig",
-		"folke/neoconf.nvim",
-		"folke/neodev.nvim",
-	},
-	config = function()
-		require("mason").setup()
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		require("neoconf").setup()
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				require("lspconfig")[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-		})
-		require("neodev").setup()
-		local lspconfig = require("lspconfig")
-		lspconfig.gopls.setup({
-			capabilities = capabilities,
-			settings = {
-				gopls = {
-					completeUnimported = true,
-					analyses = {
-						unusedparams = true,
-					},
-					staticcheck = true,
-					gofumpt = true,
-				},
-			},
-		})
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					completion = { callSnippet = "Replace" },
-				},
-			},
-		})
-	end,
+    "williamboman/mason.nvim",
+    lazy = false,
+    dependencies = {
+        {
+            "williamboman/mason-lspconfig.nvim",
+            opts = {
+                auto_install = true,
+            },
+        },
+        "neovim/nvim-lspconfig",
+        "folke/neoconf.nvim",
+        "folke/neodev.nvim",
+    },
+    config = function()
+        require("mason").setup()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        require("neoconf").setup()
+        require("mason-lspconfig").setup_handlers({
+            function(server_name)
+                require("lspconfig")[server_name].setup({
+                    capabilities = capabilities,
+                })
+            end,
+        })
+        require("neodev").setup()
+        local lspconfig = require("lspconfig")
+        lspconfig.gopls.setup({
+            capabilities = capabilities,
+            settings = {
+                gopls = {
+                    completeUnimported = true,
+                    analyses = {
+                        unusedparams = true,
+                    },
+                    staticcheck = true,
+                    gofumpt = true,
+                },
+            },
+        })
+        lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    completion = { callSnippet = "Replace" },
+                },
+            },
+        })
+    end,
 }
