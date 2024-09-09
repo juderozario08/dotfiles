3bd1541b43ff9b1f96530f2c0be966d2d318fa99
Jude Rozario <juderzro08@gmail.com>
2024-09-09 08:46:37 -0400

    Updated

--- install.sh
+++ install.sh
@@ -201,7 +201,7 @@ fi
 ~/dotfiles/symlink.sh ~/dotfiles/p10k.zsh ~/.p10k.zsh
 ~/dotfiles/symlink.sh ~/dotfiles/gitconfig/gitconfig ~/.gitconfig
 
-cp -r ~/dotfiles/OHMYZSHCUSTOM ~/.oh-my-zsh
+cp -r ~/dotfiles/ZSHCUSTOM ~/.oh-my-zsh
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 
