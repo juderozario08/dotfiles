02c263da4c80f15f9181b9ab74a37207ed3cc331
Jude Rozario <juderzro08@gmail.com>
2024-09-09 08:36:46 -0400

    Trying to add name and id generation

--- config/nvim/lua/jude/lazy/completions.lua
+++ config/nvim/lua/jude/lazy/completions.lua
@@ -32,11 +32,13 @@ return {
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                    ["C-b"] = cmp.mapping.scroll_docs(-4),
+                    ["C-f"] = cmp.mapping.scroll_docs(4),
+                    ["C-Space"] = cmp.mapping.complete(),
+                    ["C-e"] = cmp.mapping.abort(),
+                    ["C-n"] = cmp.mapping.select_next_item({ select = false }),
+                    ["C-p"] = cmp.mapping.select_prev_item({ select = false }),
+                    ["C-y>"] = cmp.mapping.confirm({ select = true }),
                 }),
                 sources = cmp.config.sources({
                     {
