0b191a622ff3b09119601bd4a16dbbe2fdd316fc
Jude Rozario <juderzro08@gmail.com>
2024-10-28 21:46:02 -0400

    Updated

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # brew setup
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-#echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
 source ~/.zshrc
 
 # cargo
@@ -12,13 +12,16 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 #dependencies
 sudo pacman -S --needed git base-devel
 cd ~
+git clone https://aur.archlinux.org/yay.git
+cd yay
+mkpkg -si
 
-sudo pacman -S fd ripgrep npm eza zoxide htop tokei tree bat fzf lazygit tmux --noconfirm
+sudo pacman -S fd ripgrep npm eza zoxide htop tokei tree bat fzf lazygit tmux btop --noconfirm
 brew install fd ripgrep npm eza zoxide htop tokei tree bat fzf zsh gh tmux
 cargo install alacritty vivid
 yay -S wezterm-git alacritty webcord spotify obsidian obs-studio zen-browser-bin firefox
-yay -S dconf dunst fastfetch gtk3 gtk4 hyprland nautilus pavucontrol
-yay -S pulse qt6ct rofi starship systemd waybar wal wlogout xsettingsd sddm
+yay -S gtk3 gtk4 hyprland nautilus pavucontrol
+yay -S pulse qt6ct rofi starship systemd waybar wlogout xsettingsd sddm
 
 cd ~
 git clone https://github.com/junegunn/fzf-git.sh
