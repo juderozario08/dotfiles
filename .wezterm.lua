bbc83ca6847f7f8e4d3a8452578016b9c60ce05e
Jude Rozario <juderzro08@gmail.com>
2024-10-31 07:08:03 -0400

    Updated

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,7 +7,11 @@ if wezterm.config_builder then
 end
 
 config.color_scheme = "Catppuccin Mocha (Gogh)"
-config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font = wezterm.font_with_fallback{
+    "FiraCode Nerd Font Mono",
+    "CaskaydiaMono Nerd Font Mono",
+    "JetBrainsMono Nerd Font"
+}
 config.font_size = 12
 config.automatically_reload_config = true
 config.window_background_opacity = 0.80
@@ -21,5 +25,6 @@ config.window_padding = {
 }
 config.term = "xterm-256color"
 config.enable_wayland = true
+config.warn_about_missing_glyphs = false
 
 return config
