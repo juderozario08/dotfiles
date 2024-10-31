2631ff20377ec59dfe963cd2132566a8e76fbc9f
Jude Rozario <juderzro08@gmail.com>
2024-10-30 22:24:03 -0400

    Updated

--- install.sh
+++ install.sh
@@ -14,21 +14,15 @@ sudo pacman -S --needed git base-devel
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
-mkpkg -si
+makepkg -si
 
 sudo pacman -S fd ripgrep npm eza zoxide htop tokei tree bat fzf lazygit tmux btop --noconfirm
 brew install fd ripgrep npm eza zoxide htop tokei tree bat fzf zsh gh tmux
 cargo install alacritty vivid
-yay -S wezterm-git alacritty webcord spotify obsidian obs-studio zen-browser-bin firefox
-yay -S gtk3 gtk4 hyprland nautilus pavucontrol
-yay -S pulse qt6ct rofi starship systemd waybar wlogout xsettingsd sddm
+yay -S gtk3 gtk4 hyprland nautilus pavucontrol pulse qt6ct rofi starship systemd waybar wlogout xsettingsd sddm hyprpaper hyprlock hypridle hyprshot swaync wezterm-git alacritty webcord obsidian obs-studio zen-browser-bin sddm-sugar-dark --noconfirm
 
 cd ~
 git clone https://github.com/junegunn/fzf-git.sh
 
-source ~/.zshrc
-
 sudo pacman -Syu --noconfirm
 yay -Syu --noconfirm
-
-gh auth login
