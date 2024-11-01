74f73b8679f56d837eea96b755c07f85cf132a0e
Jude Rozario <juderzro08@gmail.com>
2024-10-31 22:05:58 -0400

    updated

--- install.sh
+++ install.sh
@@ -2,27 +2,67 @@
 
 # brew setup
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
-source ~/.zshrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.zshrc
+source "$HOME"/.zshrc || exit
 
 # cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-. "$HOME/.cargo/env"
+. "$HOME"/.cargo/env
 
 #dependencies
 sudo pacman -S --needed git base-devel
-cd ~
+cd ~ || exit
 git clone https://aur.archlinux.org/yay.git
-cd yay
+cd yay || exit
 makepkg -si
 
-sudo pacman -S fd ripgrep npm eza zoxide htop tokei tree bat fzf lazygit tmux btop --noconfirm
+sudo pacman -S fd ripgrep npm eza zoxide htop tokei tree bat fzf lazygit tmux btop pyenv --noconfirm
 brew install fd ripgrep npm eza zoxide htop tokei tree bat fzf zsh gh tmux
 cargo install alacritty vivid
-yay -S gtk3 gtk4 hyprland nautilus pavucontrol pulse qt6ct rofi starship systemd waybar wlogout xsettingsd sddm hyprpaper hyprlock hypridle hyprshot swaync wezterm-git alacritty webcord obsidian obs-studio zen-browser-bin sddm-sugar-dark --noconfirm
+yay -S gtk3 gtk4 hyprland nautilus pavucontrol pulse qt6ct rofi starship systemd waybar wlogout xsettingsd \
+    sddm hyprpaper hyprlock hypridle hyprshot hyprutils swaync wezterm-git alacritty webcord obsidian obs-studio \
+    zen-browser-bin sddm-sugar-dark mpvpaper --noconfirm
 
-cd ~
+cd ~ || exit
 git clone https://github.com/junegunn/fzf-git.sh
 
 sudo pacman -Syu --noconfirm
 yay -Syu --noconfirm
+
+for i in $(eza -a ~/dotfiles/config/ || exit); do
+    rm -rf "$HOME/.config/${i}" 2>/dev/null
+    ln -s "$HOME/dotfiles/config/${i}" "$HOME/.config/$i"
+done
+
+rm -rf ~/.bashrc 2>/dev/null
+ln -s "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
+rm -rf ~/.zshrc 2>/dev/null
+ln -s "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
+rm -rf ~/.icons 2>/dev/null
+ln -s "$HOME/dotfiles/.icons" "$HOME/.icons"
+rm -rf ~/.themes 2>/dev/null
+ln -s "$HOME/dotfiles/.themes" "$HOME/.themes"
+rm -rf ~/.tmux.conf 2>/dev/null
+ln -s "$HOME/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
+rm -rf ~/.tmux 2>/dev/null
+ln -s "$HOME/dotfiles/.tmux" "$HOME/.tmux"
+rm -rf ~/tpm 2>/dev/null
+ln -s "$HOME/dotfiles/tpm" "$HOME/tpm"
+rm -rf ~/.vimrc 2>/dev/null
+ln -s "$HOME/dotfiles/.vimrc" "$HOME/.vimrc"
+rm -rf ~/.wezterm.lua 2>/dev/null
+ln -s "$HOME/dotfiles/.wezterm.lua" "$HOME/.wezterm.lua"
+rm -rf ~/.gitconfig 2>/dev/null
+ln -s "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
+rm -rf ~/wallpaper 2>/dev/null
+ln -s "$HOME/dotfiles/wallpaper" "$HOME/wallpaper"
+rm -rf ~/.Xresources 2>/dev/null
+ln -s "$HOME/dotfiles/.Xresources" "$HOME/.Xresources"
+rm -rf ~/.gtkrc-2.0 2>/dev/null
+ln -s "$HOME/dotfiles/.gtkrc-2.0" "$HOME/.gtkrc-2.0"
+
+sudo cp "$HOME"/.config/rofi/colors/tokyonight.rasi /usr/share/rofi/themes/
+sudo cp "$HOME"/.config/rofi/colors/catppuccin.rasi /usr/share/rofi/themes/
+sudo cp "$HOME"/.config/rofi/colors/cyberpunk.rasi /usr/share/rofi/themes/
+
+source "$HOME"/.zshrc
